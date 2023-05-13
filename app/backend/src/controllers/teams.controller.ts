99be2d09668d608d55c4b909fe14391ba1fb51de
liciniopsj <licinio.psj@gmail.com>
2023-05-12 21:54:21 -0300

    teams controller, get teams and teams by id

---
+++ app/backend/src/controllers/teams.controller.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+import TeamsService from '../services/teams.service';
+
+export default class TeamsController {
+  static async findAll(_req: Request, res: Response) {
+    const data = await TeamsService.findAll();
+    return res.json(data);
+  }
+
+  static async findById(req: Request, res: Response) {
+    const { id } = req.params;
+    const data = await TeamsService.findById(id);
+    if (!data) return res.status(404).json({ message: 'Team Not Found' });
+    return res.json(data);
+  }
+}
