b16018bfc067bf3cce0a4a4bb1df0225a739d4e8
liciniopsj <licinio.psj@gmail.com>
2023-05-13 11:38:16 -0300

    team route get all sanity check

--- app/backend/src/controllers/teams.controller.ts
+++ app/backend/src/controllers/teams.controller.ts
@@ -10,7 +10,7 @@ export default class TeamsController {
   static async findById(req: Request, res: Response) {
     const { id } = req.params;
     const data = await TeamsService.findById(id);
-    if (!data) return res.status(404).json({ message: 'Team Not Found' });
+    // if (!data) return res.status(404).json({ message: 'Team Not Found' });
     return res.json(data);
   }
 }
