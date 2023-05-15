3391cce50b573490e5bb6c18c7aa365cfd93b801
liciniopsj <licinio.psj@gmail.com>
2023-05-15 16:52:59 -0300

    response interface service fix

--- app/backend/src/services/login.service.ts
+++ app/backend/src/services/login.service.ts
@@ -2,12 +2,13 @@ import { ParamsDictionary } from 'express-serve-static-core';
 import * as bcrypt from 'bcryptjs';
 import UserService from './user.service';
 import { tokenGeneration } from '../utils/auth';
+import IRes from '../interfaces/IRes';
 
 const userFieldsRequired = { status: 400, message: 'All fields must be filled' };
 const badInput = { status: 401, message: 'Invalid email or password' };
 
 export default class LoginService {
-  static async login(payload: ParamsDictionary): Promise<object | undefined> {
+  static async login(payload: ParamsDictionary): Promise<IRes> {
     const { email, password } = payload;
     const emailRegex = email.match(/^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i);
 
@@ -24,7 +25,7 @@ export default class LoginService {
     return { status: 200, token: { token: tokenGeneration(user.email) } };
   }
 
-  static async findRole(payload: ParamsDictionary): Promise<object | undefined> {
+  static async findRole(payload: ParamsDictionary): Promise<IRes> {
     const { email } = payload;
     const user = await UserService.findOne(email);
     if (!user) return { status: 401, role: { message: 'User not found' } };
