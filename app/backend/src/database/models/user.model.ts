203c0fc0d40225e9ea4219bb596437d812e7d592
liciniopsj <licinio.psj@gmail.com>
2023-05-15 16:41:54 -0300

    migration and usermodel

---
+++ app/backend/src/database/models/user.model.ts
@@ -0,0 +1,40 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+export default class Users extends Model {
+  declare id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+Users.init({
+  id: {
+    allowNull: true,
+    primaryKey: true,
+    autoIncrement: true,
+    type: INTEGER,
+  },
+  username: {
+    allowNull: false,
+    type: STRING,
+  },
+  role: {
+    allowNull: false,
+    type: STRING,
+  },
+  email: {
+    allowNull: false,
+    type: STRING,
+  },
+  password: {
+    allowNull: false,
+    type: STRING,
+  },
+}, {
+  sequelize: db,
+  tableName: 'users',
+  modelName: 'Users',
+  timestamps: false,
+});
