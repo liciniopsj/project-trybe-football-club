9d29cc8df6716565f9c4c85998234a2eec48c890
liciniopsj <licinio.psj@gmail.com>
2023-05-18 20:54:15 -0300

    Leaderboard interface index import

--- app/backend/src/interfaces/index.ts
+++ app/backend/src/interfaces/index.ts
@@ -2,7 +2,8 @@ import ITeam from './ITeam';
 import IRes from './IRes';
 import IScore from './IScore';
 import IMatch from './IMatch';
+import ILeaderBoard from './ILeaderboard';
 
 export default ITeam;
 
-export { IRes, IScore, IMatch };
+export { IRes, IScore, IMatch, ILeaderBoard };
