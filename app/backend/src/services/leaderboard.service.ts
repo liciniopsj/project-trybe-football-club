557188249d7652bbe264f30ec42de8ea055f1a1f
liciniopsj <licinio.psj@gmail.com>
2023-05-18 20:55:43 -0300

    leaderboard service

---
+++ app/backend/src/services/leaderboard.service.ts
@@ -0,0 +1,12 @@
+import Team from '../database/models/teams.model';
+import Matches from '../database/models/matches.model';
+import { ILeaderBoard } from '../interfaces';
+import Leaderboard from '../utils/Leaderboard';
+
+export default class LeaderboardService {
+  static async pathLeaderboard(): Promise<ILeaderBoard[]> {
+    const matches = await Matches.findAll({ where: { inProgress: false } });
+    const teams = await Team.findAll();
+    return Leaderboard.buildLeaderboard(matches, teams);
+  }
+}
